fea6a52f36b408fd752b72220bf74c9b30829127
Bogdan Mitrache <bogdan@advancedinstaller.com>
2013-02-20 16:02:46 +0200

    C# custact to remove RO flag from "desktop.ini".
    "Warning 1910" may appear when you uninstall your MSI package on Vista
    or newer OS-es. This happens because somehow the file "desktop.ini" from
    the Public Desktop folder got set to Read-Only, which should not happen.
    
    This VS 2010 solution generates a DLL called
    "RemoveReadOnlyAttribute.CA.dll" that can be used as a custom action to
    remove this flag, if found to be set.
    
    To build the project you also need Wix Toolset 3.5 or higher installed,
    follow the steps from here for more details:
    http://www.advancedinstaller.com/user-guide/qa-c-sharp-ca.html

---
+++ RemoveReadOnlyAttribute/RemoveReadOnlyAttribute/CustomAction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Deployment.WindowsInstaller;
+
+using System.Windows.Forms;
+
+namespace RemoveReadOnlyAttribute
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult RemoveReadOnlyAttributeCA(Session session)
+        {
+            // display message to allow attaching the debugger
+            // MessageBox.Show("Please attach a debugger to rundll32.exe.", "Attach");
+ 
+
+            session.Log("Begin RemoveReadOnlyAttributeCA");
+
+            string desktopDir = System.Environment.GetFolderPath(Environment.SpecialFolder.CommonDesktopDirectory);
+            string filePath   = desktopDir + "\\desktop.ini"; // full path of "desktop.ini" system file 
+
+            // Create the file if it exists.
+            if (!File.Exists(filePath)) 
+            {
+                session.Log("File \"desktop.ini\" not found. Exit custom action with success.");
+                return ActionResult.Success;
+            }
+
+            session.Log("File \"desktop.ini\" found at:" + filePath);
+
+            System.IO.FileAttributes attributes = File.GetAttributes(filePath);
+
+            if ((attributes & System.IO.FileAttributes.ReadOnly) == System.IO.FileAttributes.ReadOnly)
+            {
+                // Make the file RW
+                attributes = RemoveAttribute(attributes, System.IO.FileAttributes.ReadOnly);
+                File.SetAttributes(filePath, attributes);
+                session.Log(" Read-only attribute removed for file\"desktop.ini\".");
+            }
+
+            session.Log("Exit custom action RemoveReadOnlyAttributeCA with success.");
+            return ActionResult.Success;
+        }
+
+        private static System.IO.FileAttributes RemoveAttribute(System.IO.FileAttributes attributes, System.IO.FileAttributes attributesToRemove)
+        {
+            return attributes & ~attributesToRemove;
+        }      
+    }
+}
